3586c76831fc0e69801f730629442bc4d161e48f
Ilito Torquato <ilitotor@gmail.com>
2012-09-14 20:25:21 -0300

    node tots

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	var segundo = req.body.secs;
+	var minuto = req.body.mins;
+	res.render('index', { title: 'Round Timer', segundo: segundo, minuto: minuto})
+};
+
+exports.config = function(req, res){
+  res.render('config', { title: 'ooooi' })
+};
